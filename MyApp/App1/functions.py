617089f6e006be7747a13562ec3623f919ed5630
zhang1973 <zhang1973@gmail.com>
2025-11-26 22:21:22 -0800

    delete file path variable

--- MyApp/App1/functions.py
+++ MyApp/App1/functions.py
@@ -1,8 +1,5 @@
-#import os
-#script_dir = os.path.dirname(os.path.abspath(__file__))
-#FILEPATH = os.path.join(script_dir, "todos.txt")
-from pathlib import Path
-FILEPATH = Path(__file__).resolve().parent/"todos.txt"
+FILEPATH = "todos.txt"
+
 def get_todos(filepath=FILEPATH):
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
@@ -12,4 +9,6 @@ def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
-
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
