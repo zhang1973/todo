a4fdcf7e5b31a15a56eeaa6662695f0c90af8af7
Yan Zhang <zhang1973@gmail.com>
2025-11-26 20:10:42 -0800

    delete file path variable

--- WebApp1/functions.py
+++ WebApp1/functions.py
@@ -1,9 +1,10 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
